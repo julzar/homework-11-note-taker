30d04cfd19b3677bf4c843fb919dde73952e8d64
juz <electronicallyreached@gmail.com>
2020-04-21 16:26:20 -0500

    added get routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,12 +9,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, '/public/assets')));
 
+// Get routes
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname,'/public/index.html'));
-  });
+  res.sendFile(path.join(__dirname,'/public/index.html'));
+});
+
+app.get('/notes', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', function(req, res) {
+  return res.json(JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8')));
+});
 
 
 
+
+// Start app
 app.listen(PORT, function() {
-    console.log(`-- App listening on port ${PORT} --`)
-  });
+  console.log(`-- App listening on port ${PORT} --`);
+});
