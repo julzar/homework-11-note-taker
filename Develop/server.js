88eb6664371eacc463267bcfe7a5fb4a2a638ebd
juz <electronicallyreached@gmail.com>
2020-04-22 02:45:17 -0500

    added post

--- Develop/server.js
+++ Develop/server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, '/public/assets')));
 
-// Get routes
+// Get
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname,'/public/index.html'));
 });
@@ -19,9 +19,36 @@ app.get('/notes', function(req, res) {
 });
 
 app.get('/api/notes', function(req, res) {
-  return res.json(JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8')));
+  fs.readFile('db/db.json', 'utf8', function (err, data) {
+    if (err) {
+      throw err
+    }
+    res.json(JSON.parse(data))
+  })
 });
 
+// Post
+app.post('/api/notes', function(req, res) {
+  fs.readFile('db/db.json', 'utf8', function(err, data) {
+    if (err) {
+      throw err
+    }
+
+    const notes = JSON.parse(data);
+    const newNote = req.body;
+ 
+    notes.push(newNote);
+    notes.forEach( (note, i) =>  note.id = i );
+
+    fs.writeFile('db/db.json', JSON.stringify(notes, null, 1), function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+
+    res.json(newNote);
+  });
+});
 
 
 
@@ -29,3 +56,5 @@ app.get('/api/notes', function(req, res) {
 app.listen(PORT, function() {
   console.log(`-- App listening on port ${PORT} --`);
 });
+
+
